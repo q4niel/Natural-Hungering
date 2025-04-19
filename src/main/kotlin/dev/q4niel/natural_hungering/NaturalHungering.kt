76c85af101407ee2bdb299f36b071edae0df8b8e
q4niel <120566282+q4niel@users.noreply.github.com>
2025-04-19 12:58:25 +0200

    [kt-reimpl-of-0.2.0] (#6)
    - passive exhaustion & exhaustion divider

--- src/main/kotlin/dev/q4niel/natural_hungering/NaturalHungering.kt
+++ src/main/kotlin/dev/q4niel/natural_hungering/NaturalHungering.kt
@@ -6,11 +6,11 @@ import org.slf4j.LoggerFactory
 
 object NaturalHungering: ModInitializer {
 	const val MOD_ID: String = "natural_hungering";
+	const val EXHAUSTION_DIVIDER: Float = 100f;
+	const val PASSIVE_EXHAUSTION: Float = .001f;
 
 	fun print(value: String): Unit = _logger.info(value);
     private val _logger: Logger = LoggerFactory.getLogger(MOD_ID)
 
-	override fun onInitialize() {
-		print("Hello Fabric world!")
-	}
+	override fun onInitialize() {}
 }
