6656dc424f0b4dd09c7a104a9235d7b7d89621f6
q4niel <github.obscure938@passmail.net>
2025-10-19 18:31:54 +0200

    initialize new fabric mod template

--- src/main/kotlin/dev/q4niel/NaturalHungeringDataGenerator.kt
+++ src/main/kotlin/dev/q4niel/NaturalHungeringDataGenerator.kt
@@ -1,8 +1,8 @@
-package dev.q4niel.natural_hungering
+package dev.q4niel
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
 
-object NaturalHungeringDataGenerator: DataGeneratorEntrypoint {
+object NaturalHungeringDataGenerator : DataGeneratorEntrypoint {
 	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {}
 }
